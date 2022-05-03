f41470cddcef517ee08a3f3ff9b7b797988bf27a
RainyKnight <stoltmanlucas@gmail.com>
2022-05-02 17:58:13 -0700

    Change Catalan.py into recursive and take arguments from terminal

--- catalan.py
+++ catalan.py
@@ -4,20 +4,23 @@
 # CSS 340 A, Dimpsey
 # Version: 1.0
 
+import sys
+
+
 def catalan(num: int):
     # base case
-    if num <= 1:
+    if num == 0 or num == 1:
         return 1
+    if num < 0:
+        return None
 
-    # memoization to save time
-    memory = [0 for i in range(num + 1)]
+    result = 0
+    for i in range(num):
+        result += catalan(i) * catalan(num - i - 1)
 
-    # set the first two values from the base case
-    memory[0] = memory[1] = 1
+    return result
 
-    for i in range(2, num + 1):
-        memory[i] = 0
-        for j in range(i):
-            memory[i] += memory[j] * memory[i - j - 1]
 
-    return memory[num]
+argument = int(sys.argv[1])
+
+print(catalan(argument))
--- main.py
+++ main.py
@@ -14,9 +14,13 @@ print("\n-------Negative-------")
 for i in range(-9, 0):
     print(f"n({i}) : {catalan(i)}")
 
+# TODO floats
+# print("\n-------Floats-------")
+
+
 print("\n-------Efficiency-------")
-n = 100
+n = 10
 print(f"n({n})")
 t = timeit.Timer(f"catalan({n})", "from catalan import catalan")
-duration = t.timeit(100)
+duration = t.timeit(1000)
 print("Time:", duration, "ms")
